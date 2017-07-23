14907b437e4cef51787a269d20337ae0c3194bfe
Daniel Bonham <bonhamdaniel@gmail.com>
2017-07-23 14:54:16 -0400

    Update settings.py
    Updated database configuration for Heroku.

--- benchwarmer/settings.py
+++ benchwarmer/settings.py
@@ -131,3 +131,8 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
    }
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
